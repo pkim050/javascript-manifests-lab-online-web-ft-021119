8c3c8ad472f1cf53ac5f66dd97f34e9224ccafbd
Patrick Kim <pkim050@gmail.com>
2019-06-12 14:39:04 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require wrong_place_wrong_time
+//= require alert_maker
+//= require doesn_nothing
+//= require hello_world
+//= require learn
+//= require main
